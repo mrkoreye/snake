cb1663931689dbd4554cdcb447985ba1314cac20
Korey Kassir <mrkoreye@gmail.com>
2017-09-18 11:36:39 -0700

    refactored a bit, removed external library dependencies

--- snake.js
+++ snake.js
@@ -1,95 +1,95 @@
-//var _ = require('./underscore')
-
-Snakey = {};
+window.Snakey = {
+	boardSize: 30
+};
 
 Snakey.Snake = (function () {
 	function Snake () {
 		this.snake = [[5, 5], [5, 6], [5, 7], [5, 8], [5, 9]];
-		this.head = _.last(this.snake);
-		this.direction = "down";
+		this.head = this.snake[this.snake.length - 1];
+		this.direction = 'down';
 	}
 	
-	Snake.prototype.turn = function (direction) {
-		
+	Snake.prototype.turn = function(direction) {
 		switch(direction) {
-		case "left":
-			if(!(this.direction == "right")) {
-				this.direction = direction;
-			}
-			break;
-		case "right":
-			if(!(this.direction == "left")) {
-				this.direction = direction;
-			}
-			break;
-		case "up":
-			if(!(this.direction == "down")) {
-				this.direction = direction;
-			}
-			break;
-		case "down":
-			if(!(this.direction == "up")) {
-				this.direction = direction;
-			}
-			break;
+			case 'left':
+				if(!(this.direction == 'right')) {
+					this.direction = direction;
+				}
+				break;
+			case 'right':
+				if(!(this.direction == 'left')) {
+					this.direction = direction;
+				}
+				break;
+			case 'up':
+				if(!(this.direction == 'down')) {
+					this.direction = direction;
+				}
+				break;
+			case 'down':
+				if(!(this.direction == 'up')) {
+					this.direction = direction;
+				}
+				break;
 		}
 	}
 	
-	Snake.prototype.step = function () {
+	Snake.prototype.step = function() {
 		var newHead = [];
 		
 		switch(this.direction) {
-		case "up":
-			newHead[0] = this.head[0] - 1;
-			newHead[1] = this.head[1];
-			this.changeSnake(newHead);
-			break;
-		case "down":
-			newHead[0] = this.head[0] + 1;
-			newHead[1] = this.head[1];
-			this.changeSnake(newHead);
-			break;
-		case "left":
-			newHead[0] = this.head[0];
-			newHead[1] = this.head[1] - 1;
-			this.changeSnake(newHead);
-			break;
-		case "right":
-			newHead[0] = this.head[0];
-			newHead[1] = this.head[1] + 1;
-			this.changeSnake(newHead);
-			break;
-		default:
-			//should never get here...
+			case 'up':
+				newHead[0] = this.head[0] - 1;
+				newHead[1] = this.head[1];
+				this.changeSnake(newHead);
+				break;
+			case 'down':
+				newHead[0] = this.head[0] + 1;
+				newHead[1] = this.head[1];
+				this.changeSnake(newHead);
+				break;
+			case 'left':
+				newHead[0] = this.head[0];
+				newHead[1] = this.head[1] - 1;
+				this.changeSnake(newHead);
+				break;
+			case 'right':
+				newHead[0] = this.head[0];
+				newHead[1] = this.head[1] + 1;
+				this.changeSnake(newHead);
+				break;
+			default:
+				//should never get here...
 		}
 	}
 	
 	Snake.prototype.changeSnake = function(newHead) {
 		this.snake.push(newHead);
 		this.snake.shift();
-		this.head = _.last(this.snake);
+		this.head = this.snake[this.snake.length - 1];
 	}
 	
 	Snake.prototype.offBoard = function () {
-		var min = _.min(this.head);
-		var max = _.max(this.head);
-		if(max > 29 || min < 0) {
+		var overMax = (this.head[0] >= Snakey.boardSize) || (this.head[1] >= Snakey.boardSize);
+		var underMin = (this.head[0] < 0) || (this.head[1] < 0);
+
+		if (overMax || underMin) {
 			return true;
 		} else {
 			return false;
 		}
 	}
 		
-	Snake.prototype.biggerSnake = function () {
-	    var last = _.first(this.snake);
-      for (var i = 0; i < 2; i++) {
-          this.snake.unshift([last[0] + i + 1, last[1]]);
-      }
+	Snake.prototype.embiggenSnake = function () {
+		var endOfSnake = this.snake[0];
+		for (var i = 0; i < 2; i++) {
+			this.snake.unshift([endOfSnake[0] + i + 1, endOfSnake[1]]);
+		}
 	}
 	
 	Snake.prototype.hitSelf = function () {
 		for (var i = 0, len = this.snake.length; i < (len - 1); i++){
-			if(this.head.toString() === this.snake[i].toString()){
+			if (this.head.toString() === this.snake[i].toString()) {
 				return true;
 			} 
 		}
@@ -106,18 +106,20 @@ Snakey.Board = (function () {
 	
 	Board.prototype.update = function(snake) {
 		this.clearBoard();
-		for(var i = 0, len = snake.snake.length; i < len; i ++) {
+		for (var i = 0, len = snake.snake.length; i < len; i ++) {
 			var row = snake.snake[i][0];
 			var col = snake.snake[i][1];
-			this.board[row][col] = "snake";
+			if (this.board[row]) {
+				this.board[row][col] = 'snake';
+			}
 		}
 	}
 	
 	function createBoard () {
 		var board = [];
-		for (var i = 0; i < 30; i++) {
+		for (var i = 0; i < Snakey.boardSize; i++) {
 			var row = [];
-			for(var j = 0; j < 30; j++) {
+			for (var j = 0; j < Snakey.boardSize; j++) {
 				row.push(null);
 			}
 			board.push(row);
@@ -127,9 +129,9 @@ Snakey.Board = (function () {
 	}
 	
 	Board.prototype.clearBoard = function () {
-		for (var i = 0; i < 30; i++) {
-			for(var j = 0; j < 30; j++) {
-				if(this.board[i][j] == "snake") {
+		for (var i = 0; i < Snakey.boardSize; i++) {
+			for (var j = 0; j < Snakey.boardSize; j++) {
+				if (this.board[i][j] == 'snake') {
 					this.board[i][j] = null;
 				};
 			}
@@ -140,9 +142,8 @@ Snakey.Board = (function () {
 })();
 
 Snakey.Game = (function () {
-	
 	function getRandomInt (min, max) {
-	    return Math.floor(Math.random() * (max - min + 1)) + min;
+	  return Math.floor(Math.random() * (max - min + 1)) + min;
 	}
 	
 	function Game () {
@@ -153,22 +154,19 @@ Snakey.Game = (function () {
 	
 	Game.prototype.step = function () {
 		this.snake.step();
-		// if(this.snake.hitSelf() || this.snake.offBoard()) {
-// 			clearInterval(timer);
-// 		};
 		this.addApple();
 		this.eatApple();
 		this.board.update(this.snake);
-		
 	}
 	
 	Game.prototype.addApple = function() {
 		var num = Math.random();
 		var row = getRandomInt(0, 29);
 		var col = getRandomInt(0, 29); 
-		if(num > 0.95) {
-			if(this.board.board[row][col] == null) {
-				this.board.board[row][col] = "apple";
+
+		if (num > 0.95) {
+			if (this.board.board[row][col] == null) {
+				this.board.board[row][col] = 'apple';
 			}
 		}
 	}
@@ -176,10 +174,11 @@ Snakey.Game = (function () {
 	Game.prototype.eatApple = function() {
 		var snakeR = this.snake.head[0];
 		var snakeC = this.snake.head[1];
-		if(this.board.board[snakeR][snakeC] == "apple") {
+
+		if (this.board.board[snakeR] && this.board.board[snakeR][snakeC] == 'apple') {
 			this.points += 1;
-			this.board.board[snakeR][snakeC] = "snake";
-			this.snake.biggerSnake();
+			this.board.board[snakeR][snakeC] = 'snake';
+			this.snake.embiggenSnake();
 		};
 	}
 	
--- snake_ui.js
+++ snake_ui.js
@@ -1,86 +1,92 @@
-$(function() {
-	Game = new Snakey.Game();
-	timer = '';
+(function() {
+	var Game = new Snakey.Game();
+	var SnakeTimer = '';
+	var across = 30;
+	var down = 30;
 	
 	function createGrid() {
-		$('.master').html('');
-		var across = 30;
-		var down = 30;
+		document.getElementById('root-div').innerHTML = '';
 		for (var j = 0; j < across; j++) {
-			$('.master').append($('<div class="row-snake" id="row' + j + '">'));
+			var snakeRowElement = document.createElement('div');
+			snakeRowElement.id = 'row' + j;
+			snakeRowElement.classList.add('row-snake');
+			document.getElementById('root-div').appendChild(snakeRowElement);
+
 			for (var i = 0; i < down; i++) {
-				$('#row' + j).append($('<div class="space" id="space' + i + "_" + j + '">&nbsp;</div>'));
+				var spaceElement = document.createElement('div');
+				spaceElement.id = 'space' + i + "_" + j;
+				spaceElement.classList.add('space');
+				spaceElement.innerHTML = '&nbsp;'
+				document.getElementById('row' + j).appendChild(spaceElement);
 			}
 		}
 	}
 
-	
 	function renderSnake() {
-		var across = 30;
-		var down = 30;
 		for (var i = 0; i < across; i++) {
 			for (var j = 0; j < down; j++) {
+				var element = document.getElementById('space' + i + "_" + j);
 				switch (Game.board.board[i][j]) {
-				case "snake":
-					$('#space' + i + "_" + j).removeClass("apple").addClass("snake");
-					break;
-				case null:
-					$('#space' + i + "_" + j).removeClass("snake apple");
-					break;
-				case "apple":
-					$('#space' + i + "_" + j).addClass("apple");
-					break;
+					case 'snake':
+						element.classList.remove('apple')
+						element.classList.add('snake');
+						break;
+					case null:
+						element.classList.remove('snake');
+						element.classList.remove('apple');
+						break;
+					case 'apple':
+						element.classList.add('apple');
+						break;
 				}
 			}
 		}
 		
-		$('#messages').text("Points: " + Game.points);
+		document.getElementById('messages').textContent = 'Points: ' + Game.points;
 	}
 
-	createGrid();
-	$('#messages').text("Press Spacebar to start/restart");
+	function play() {
+		Game = new Snakey.Game();
+		createGrid();
+		SnakeTimer = window.setInterval(function() {
+			Game.step();
+			if (Game.snake.hitSelf() || Game.snake.offBoard()) {
+				clearInterval(SnakeTimer);
+			} else {
+				renderSnake();
+			}
+		}, 100);
+	}; 
 	
-	$(document).keydown(function (e) {
-		
+	document.addEventListener('keydown', function(e) {
 	  var keyCode = e.keyCode;
 
 		if([32, 37, 38, 39, 40].indexOf(keyCode) > -1) {
-        e.preventDefault();
+      e.preventDefault();
     }
 
 	  switch (keyCode) {
+			// Spacebar
 			case 32:
-				if(timer != '') {
-					clearInterval(timer);
+				if (SnakeTimer) {
+					clearInterval(SnakeTimer);
 				}
 				play();
 	    case 37:
-	      Game.snake.turn("up");
+	      Game.snake.turn('up');
 	    	break;
 	    case 38:
-	      Game.snake.turn("left");
+	      Game.snake.turn('left');
 	    	break;
 	    case 39:
-	      Game.snake.turn("down");
+	      Game.snake.turn('down');
 	    	break;
 	    case 40:
-	      Game.snake.turn("right");
+	      Game.snake.turn('right');
 	    	break;
 	  }
 	});
-	
 
-		play = function () {
-			Game = new Snakey.Game();
-			createGrid();
-			timer = window.setInterval(function() {
-				Game.step();
-				if(Game.snake.hitSelf() || Game.snake.offBoard()) {
-					clearInterval(timer);
-				} else {
-					renderSnake();
-				}
-							}, 100);
-	 }; 
-
-});
+	createGrid();
+	document.getElementById('messages').textContent = 'Press Spacebar to start/restart';
+})();
